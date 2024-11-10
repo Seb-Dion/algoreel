e2624b9022b7567e8c5be990d0ad990bcece1cf1
sebastiandion <swd7104@gmail.com>
2024-11-10 12:53:04 -0500

    Began on login (frontend)

---
+++ frontend/algoreel/src/pages/login/login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const Login = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  
+};
+
+export default Login;
